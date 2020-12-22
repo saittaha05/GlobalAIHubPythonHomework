97779bf0b507a1f2d923f4993f8825a34c1ac4af
Sait Şahin <76446815+saittaha05@users.noreply.github.com>
2020-12-22 18:45:08 +0300

    Add files via upload

---
+++ homework-1.py
@@ -0,0 +1,26 @@
+ad=input("adınız giriniz:")
+soyad=input("soyadınız giriniz:")
+no=int(input("telefon numaranızı giriniz:"))
+yas=int(input("yaşınızı giriniz:"))
+sehir=input("yaşadığınız şehri giriniz:")
+
+
+print(f'''
+AD : {ad}
+SOYAD : {soyad}
+Telefon numarası : {no}
+YAŞ : {yas}
+ŞEHİR : {sehir}
+''')
+
+print(f'''
+AD type : {type(ad)}
+SOYAD type : {type(soyad)}
+TELEFON NUMARASI type : {type(no)}
+YAŞ type : {type(yas)}
+ŞEHİR type : {type(sehir)}
+''')
+
+
+
+
